636fd1c2e60cff3a8e430c086f5ab3f0365afe32
Lisa Smithen <smithenlisa@gmail.com>
2019-10-27 17:58:30 -0500

    Created controllers folder and burgers_controller.js to import express and burger.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+var burger = require('../models/burger.js');
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render('index', hbsObject);
+    });
+    });
+
+router.put('/burger/:id', function(req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function(data) {
+        res.redirect('/');
+    });
+});
+
+module.exports = router;
