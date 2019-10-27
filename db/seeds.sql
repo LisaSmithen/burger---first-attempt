d24210a03bebdf14cd9303af3a8be92be936b533
Lisa Smithen <smithenlisa@gmail.com>
2019-10-26 23:24:24 -0500

    created seeds.sql and populated the burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burgers_name, devoured) VALUES ('Caribbean Chicken Burger', true);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('Whiskey King Burger', false);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('Angus Burger', true);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('Lobster Burger', true);
